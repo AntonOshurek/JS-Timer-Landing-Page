72d53301a20f1c8b51d45a112a8f453ff1960ede
AntonOshurek <oshurekweb@gmail.com>
2021-08-12 23:24:14 +0200

    add all date data to local storage and array

--- script.js
+++ script.js
@@ -1,63 +1,55 @@
 "use strict"
 
-const item = document.querySelectorAll('.timer__item > p');
-const countmainelement = document.querySelector('.timer');
-
 const deadlineDate = document.querySelector('.deadline__date');
 const daedlineTime = document.querySelector('.deadline__time');
 const setDeadlineBtn = document.querySelector('.deadline__btn');
 
-let deadDate = '';
-let deadTime = '';
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('seconds');
 
+let deadline = [];
 
 setDeadlineBtn.addEventListener('click', (e) => {
     e.preventDefault;
-    if(!deadlineDate.value || !daedlineTime.value)  {
-        setDeadlineBtn.innerText = 'huj';
-        setDeadlineBtn.classList.add('deadline__btn--error');
-        
+    if(!deadlineDate.value)  {
+        inputError()
+    } else if (deadlineDate.value && !daedlineTime.value) {
+        inputSuccess(deadlineDate.value, '0');
     } else {
-        deadDate = deadlineDate.value;
-        deadTime = daedlineTime.value;
-        console.log(deadDate);
-        console.log(deadTime);
-        setDeadlineBtn.innerText = 'data received';
-        setTimeout(() => (setDeadlineBtn.innerText = 'set timer'), 3000);
-        setDeadlineBtn.classList.remove('deadline__btn--error');
-        getCountdownTime();
+        inputSuccess(deadlineDate.value, daedlineTime.value);
     }
 })
 
-console.log(Date.parse(deadDate));
+function inputError() {
+    setDeadlineBtn.innerText = 'set date!';
+    setTimeout(() => (setDeadlineBtn.innerText = 'SET DATE!'), 3000);
+    setDeadlineBtn.classList.add('deadline__btn--error');
+    setTimeout(() => (setDeadlineBtn.classList.remove('deadline__btn--error')), 3000);
+};
 
-//let countdownDate = new Date(2028, 6, 18, 0, 0).getTime();
+function inputSuccess(dateValue, timeValue) {
+    deadline = {
+        deadDate: dateValue,
+        deadTime: timeValue
+    };
+    pushToStorage(deadline);
 
-function getCountdownTime () {
-    const now = new Date().getTime();
-    let timerDate = Date.parse(deadDate);
+    deadlineDate.value = '';
+    daedlineTime.value = '';
+    setDeadlineBtn.innerText = 'data received';
+    setTimeout(() => (setDeadlineBtn.innerText = 'set timer'), 3000);
+    setDeadlineBtn.classList.remove('deadline__btn--error');
+};
 
-    const distance = timerDate - now;
-    
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneHour = 60 * 60 * 1000;
-    const oneMinute = 60 * 1000;
+function pushToStorage(dataItem) {
+    localStorage.setItem('deadline', JSON.stringify(dataItem));
+};
 
-    let days = Math.floor(distance / oneDay);
-    let hours = Math.floor((distance % oneDay) / oneHour);
-    let minutes = Math.floor((distance % oneHour) / oneMinute);
-    let seconds = Math.floor((distance % oneMinute) / 1000);
-
-    const values = [days, hours, minutes, seconds];
-
-    item.forEach(function (item, index) {
-        item.textContent = (values[index])
-    });
-
-    if (distance < 0) {
-        clearInterval(countdown);
-        countmainelement.innerHTML = "<h4>End of time...</h4>"
+function checkStorage() {
+    if(localStorage.getItem('deadline')) {
+        deadline = JSON.parse(localStorage.getItem('deadline'));
     }
-}
-
- let countdown = setInterval(getCountdownTime, 1000);
+};
+checkStorage();
